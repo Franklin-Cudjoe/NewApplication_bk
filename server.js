d26c2d3ba6aec1a8eeaeac2200853f9a04172763
Franklin Ebenezer Cudjoe <franklin.cudjoe@dataminesoftware.com>
2025-07-10 08:15:11 +0000

    serving images

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
 app.use(bodyParser.json());
